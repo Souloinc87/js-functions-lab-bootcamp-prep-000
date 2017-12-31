cbdd22e039c7a7fc9ae09161231bd8035b4cba84
Souloinc87 <souloinc87@gmail.com>
2017-12-31 22:10:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // write your code below!
+function happyHolidays() {
+  return 'Happy holidays!'
+}
+function happyHolidaysTo(Serayah) {
+  return `Happy holidays, ${Serayah}!`
+}
+function happyHolidayTo(New Years, Serayah) {
+  return `Happy ${'holiday'}, ${'Serayah'}!`
+}
